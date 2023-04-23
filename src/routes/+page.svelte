4b024124b94a7cfeb802b205e4b17fc307d5f8e9
angelospk <rovas450@gmail.com>
2023-04-23 04:09:50 +0300

    started implementing top

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<script>
+	//display stats
+	import TopArtists from "../TopArtists.svelte";
+	// const { default: TopArtists } = require("../TopArtists.svelte");
+	import TopSongs from "../TopSongs.svelte";
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1>Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code>/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code>/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code>/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
+		<h1 class="my-5">
+			Στατιστικά Diesi 101.3
+		</h1>
+		
+		<!-- <TopArtists/> -->
+		<!-- <h2>Top Songs of the Week</h2> -->
+		<TopSongs />
 	</div>
 </div>
