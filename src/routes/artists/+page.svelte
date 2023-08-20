6dcf32678d89f3d7a80732d42031740ca9659af6
angelospk <rovas450@gmail.com>
2023-08-20 23:17:06 +0300

    fixed

--- src/routes/artists/+page.svelte
+++ src/routes/artists/+page.svelte
@@ -10,10 +10,10 @@
   </script>
   
   <!-- Add buttons -->
-  <a href="/artists/day" class="btn variant-filled hover:variant-filled-secondary">day</a>
-  <a href="/artists/week" class="btn variant-filled hover:variant-filled-secondary">week</a>
-  <a href="/artists/month" class="btn variant-filled hover:variant-filled-secondary">month</a>
-  <a href="/artists/all" class="btn variant-filled hover:variant-filled-secondary">all</a>
+  <a href="/artists/day" class="btn variant-filled hover:variant-filled-primary" >day</a>
+  <a href="/artists/week" class="btn variant-filled hover:variant-filled-primary" >week</a>
+  <a href="/artists/month" class="btn variant-filled hover:variant-filled-primary" >month</a>
+  <a href="/artists/all" class="btn variant-filled hover:variant-filled-primary" >all</a>
   <h3 class="my-5">Επέλεξε χρονικό διάστημα.</h3>
   <!-- <button on:click={() => selectTimeOption('week')}>Week</button>
   <button on:click={() => selectTimeOption('month')}>Month</button>
