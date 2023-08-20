347117cde5c90980373ee1d46c6a39eb72f7c299
angelospk <rovas450@gmail.com>
2023-08-21 00:01:32 +0300

    hided page1 and page2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,8 +25,8 @@ import {choice} from '../convertTime';
 			<a href="/" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Αρχική</a>
 			<a href="/artists" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Top Καλλιτέχνες</a>
 			<a href="/songs" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Top Τραγούδια</a>
-			<a href="/page1" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Page 1</a>
-			<a href="/page2" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Page 2</a>
+			<!-- <a href="/page1" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Page 1</a>
+			<a href="/page2" class="btn btn-sm-filled m-1" data-sveltekit-preload-data="hover">Page 2</a> -->
 			<!-- <a class="ml-3" href="/artists">Top Καλλιτέχνες</a>
 			<a class="ml-3" href="/songs">Top Τραγούδια</a> -->
 		</nav>
